32b4ff0501cb9c824cd6bc455ab6f66c337c065e
Chilengue <chilenguejoseorlando@gmail.com>
2024-08-07 09:41:11 +0200

    create-Http-localhost

--- main.go
+++ main.go
@@ -8,17 +8,32 @@ import (
 	"github.com/joho/godotenv"
 )
 
+// func main() {
+// 	err := godotenv.Load()
+// 	if err != nil {
+// 		log.Fatal("Error loading .env file")
+// 	}
+
+// 	router := gin.Default()
+
+// 	routes.InitRoutes(&router.RouterGroup)
+
+// 	if err := router.Run("8087"); err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal("Error loafing  .env file")
 	}
 
 	router := gin.Default()
 
 	routes.InitRoutes(&router.RouterGroup)
 
-	if err := router.Run("8000"); err != nil {
+	if err := router.Run(":8100"); err != nil {
 		log.Fatal(err)
 	}
 }
